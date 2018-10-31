4a773d946bf400e596c75e23669d65c756723893
Kilkee <39921163+Kilkee@users.noreply.github.com>
2018-10-31 21:15:51 +0000

    Updated food adapter file

--- FoodRater9/app/src/main/java/com/example/delaney/foodrater9/FoodAdapter.java
+++ FoodRater9/app/src/main/java/com/example/delaney/foodrater9/FoodAdapter.java
@@ -2,33 +2,82 @@ package com.example.delaney.foodrater9;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder> {
 
-    public void addFood(){
+    private List<Food> mFoods = new ArrayList<>();
+    private RecyclerView mRecyclerView;
 
-
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        mRecyclerView = recyclerView;
     }
+
+
+    public void addFood(){
+    // TODO: I do this (I'm gonna have to do a lot of things that are similar
+        // TODO: to name adapter, I'm going to have to figure this out
+
+        //**************
+        mFoods.add(0, new Food());
+        notifyItemInserted(0);
+        notifyItemRangeChanged(0, mFoods.size());
+        mRecyclerView.getLayoutManager().scrollToPosition(0);
+        //*******************
+    }
+
+    private void deleteFood(int position) {
+        mFoods.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(0, mFoods.size());
+    }
+
 
     @NonNull
     @Override
     public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int position) {
-        return null;
+        // TODO: Ido this
+    //**********
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.food_item_view, parent, false);
+        return new FoodViewHolder(itemView);
+
+    //**********
+      //  return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull FoodViewHolder foodViewHolder, int position) {
+    public void onBindViewHolder(@NonNull FoodViewHolder holder, int position) {
+    final Food food = mFoods.get(position);
+    holder.mName.setText(food.getName());
+    // TODO: I do this
+
+    //**************
+        holder.mImageView.setImageResource(food.getImageResourceId());
+        holder.mRatingBar.setRating(food.getRating());
+    //********************
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        //TODO: I do this
+
+        //********
+        return mFoods.size();
+        //*******
+
+      //  return 0;
     }
 
     class FoodViewHolder extends RecyclerView.ViewHolder{
@@ -41,6 +90,33 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder
             mImageView = itemView.findViewById(R.id.food_pic);
             mName = itemView.findViewById(R.id.name);
             mRatingBar = itemView.findViewById(R.id.rating_bar);
+
+            // TODO: Delete this food on long press // I need to do this
+
+            //**************
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    deleteFood(getAdapterPosition());
+                    return true;
+
+                }
+            });
+
+            //*****************8
+
+                    // Done together, update the rating for this food
+
+            mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                @Override
+                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                    if (fromUser){
+                        // Update this food's rating
+                        Food currentFood = mFoods.get(getAdapterPosition());
+                        currentFood.setRating(rating);
+                    }
+                }
+            });
 
         }
     }
